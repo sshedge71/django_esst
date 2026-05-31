ca07af67e684df24aafd7bae32d6781c945b8ad2
Suraj Shedge <sshedge@qti.qualcomm.com>
2026-05-31 23:29:19 +0530

    Added admin interface along with models
    Change-Id: I42727d71283a8d24913775fd79f4a223bd861df2

--- myapi/admin.py
+++ myapi/admin.py
@@ -1,6 +1,46 @@
+import csv
+from datetime import timezone
+from django.http import HttpResponse
+
+from django.http import HttpResponse
 from django.contrib import admin
 
 # Register your models here.
-from .models import Book
+from .models import Book, Author
 
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    search_fields = ('title', 'author')
+    list_filter = ('title', 'author', 'published_date')
+
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'birth_date', 'mobile', 'email')
+#     search_fields = ('name', 'email')
+#     list_filter = ('birth_date',)
+
+# admin.site.register(Book, BookAdmin)
+
+# admin.site.register(Author, list_display = ('name', 'birth_date', 'mobile', 'email'), 
+#                     search_fields = ('name', 'email'), 
+#                     list_filter = ('birth_date',))
+
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date', 'mobile', 'email')
+    search_fields = ('name', 'email')
+    list_filter = ('birth_date',)
+    
+    fieldsets = (
+    ('Personal Info', {
+        'fields': ('name', 'birth_date')
+    }),
+    ('Contact Info', {
+        'fields': ('mobile', 'email')
+    }),
+)   
+    ordering = ('name',)
+    readonly_fields = ('mobile',)
+    list_per_page = 10
--- myapi/models.py
+++ myapi/models.py
@@ -6,8 +6,18 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
-    isbn = models.CharField(max_length=13, unique=True)
 
     def __str__(self):
         return self.title
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    mobile = models.IntegerField(unique=True)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 
